18be51ee683a73dd6f78ccea0210f97f9b6af72d
lfzs <jycomputer_417@163.com>
2024-09-14 16:58:18 +0800

    doc: readme

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 base
 https://github.com/bluwy/create-vite-extra/blob/master/template-ssr-vue/server.js
-
-配合 Suspense 和 import.meta.env.SSR 实现 ssr fetch https://github.com/vitejs/vite/issues/1969
+import.meta.env.SSR 实现 useFetch.fetchData('', { ssr: true })
--- src/views/about/Index.jsx
+++ src/views/about/Index.jsx
@@ -3,7 +3,9 @@ import { defineComponent, ref } from 'vue'
 export default defineComponent({
   async setup() {
     const count = ref(0)
-    await new Promise(resolve => setTimeout(resolve, 5e3))
+    if (import.meta.env.SSR) {
+      await new Promise(resolve => setTimeout(resolve, 5e3))
+    }
     count.value = 10
 
     return () => <div>
